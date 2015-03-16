e9686dec346fa17c3faa9db16a24a45b25f9e222
Thomas Min <thomas.min.git@gmail.com>
2015-03-16 23:27:54 +0900

    Add onitemclick event handler

--- MyAlloyTabApp/app/controllers/index.js
+++ MyAlloyTabApp/app/controllers/index.js
@@ -8,25 +8,46 @@ function onClickButton2(e) {
 	Titanium.API.info("Test2");
 };
 
-var elementData = [
-	{"name":"Hydrogen" },
-	{"name":"Helium" },
-	{"name":"Lithium" },
-	{"name":"Beryllium" },
-	{"name":"Boron"	},
-	{"name":"Carbon" },
-	{"name":"Nitrogen"},
-	{"name":"Titanium.API.info(\"Test2\");"}
-];
+var elementData = [{
+	"name" : "Hydrogen"
+}, {
+	"name" : "Helium"
+}, {
+	"name" : "Lithium"
+}, {
+	"name" : "Beryllium"
+}, {
+	"name" : "Boron"
+}, {
+	"name" : "Carbon"
+}, {
+	"name" : "Nitrogen"
+}, {
+	"name" : "Titanium.API.info(\"Test2\");"
+}];
 var items = _.map(elementData, function(element) {
- return {
-		properties: {
-			title: element.name
+	return {
+		properties : {
+			title : element.name
 		}
 	};
 });
-
+$.elementsList.sections[0].setItems(items);
 
 function onItemClickList1(e) {
-	//Ti.API.infe(e.toString());
+	Titanium.API.info("e.sectionIndex=" + e.sectionIndex + ",e.itemIndex=" + e.itemIndex);
+
+	// Get the section of the clicked item
+	var section = $.elementsList.sections[e.sectionIndex];
+	// Get the clicked item from that section
+	var item = section.getItemAt(e.itemIndex);
+	Titanium.API.info("title=" + item.properties.title);
+	// Update the item's `title` property and set it's color to red:
+	//item.properties.title += " (clicked)";
+	item.properties.color = 'red';
+	// Update the item in the list
+	section.updateItemAt(e.itemIndex, item);
+
+	// FIXME for testing
+	eval(item.properties.title);
 }
