65d2600c841528ae831ebce37229d8dbed030b1b
Thomas Min <thomas.min.git@gmail.com>
2015-03-17 21:10:37 +0900

    Add row in tableview.

--- MyAlloyTabApp/app/controllers/index.js
+++ MyAlloyTabApp/app/controllers/index.js
@@ -1,13 +1,41 @@
-function doClick(e) {  
-    alert($.label.text);
+function doClick(e) {
+	alert($.label.text);
 }
- 
-function showmenu(e){
-	$.main.animate({left:200,duration:100});
+
+function showmenu(e) {
+	$.main.animate({
+		left : 200,
+		duration : 100
+	});
 }
- 
-function hidemenu(e){
-	$.main.animate({left:0,duration:100});
+
+function hidemenu(e) {
+	$.main.animate({
+		left : 0,
+		duration : 100
+	});
 }
- 
+
 $.index.open();
+
+var row = Ti.UI.createTableViewRow({
+	id : 'listRow'
+});
+
+var view = Ti.UI.createView({
+	id : 'rowContainer'
+});
+
+var statusLabel = Ti.UI.createLabel({
+	id : 'status',
+	text : 'Feeling good'
+});
+
+view.add(statusLabel);
+row.add(view);
+
+$.mainList.appendRow(row);
+
+// Ti.UI.info('row=' + $.mainList);
+
+
